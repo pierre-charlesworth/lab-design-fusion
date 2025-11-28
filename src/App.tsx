ab9946cc6b0b578cc6aeba8702515ecdd6e5ee0b
pierre-charlesworth <pierre.charlesworth@gmail.com>
2025-11-28 11:09:29 +0100

    Integrate Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import Research from "./pages/Research";
 import People from "./pages/People";
@@ -30,6 +31,7 @@ const App = () => (
           </Routes>
         </BrowserRouter>
         <SpeedInsights />
+        <Analytics />
       </TooltipProvider>
     </HelmetProvider>
   </QueryClientProvider>
